cf668499602e1217dd74d7f5d2073fba3cfa2d58
Luis Zarate <126049010+GenoXZG@users.noreply.github.com>
2026-05-28 00:31:35 -0600

    Update ExternalFileHelper.kt

--- Archivos/app/src/main/java/com/example/archivos/data/files/ExternalFileHelper.kt
+++ Archivos/app/src/main/java/com/example/archivos/data/files/ExternalFileHelper.kt
@@ -6,23 +6,18 @@ import java.io.OutputStreamWriter
 
 class ExternalFileHelper(private val context: Context) {
 
-    /**
-     * Exporta el historial completo de tickets a un archivo en el almacenamiento externo.
-     * @param uri La ruta segura generada por el Storage Access Framework.
-     * @param contenido El texto estructurado con todos los tickets.
-     */
     fun exportarTickets(uri: Uri, contenido: String): Boolean {
         return try {
-            // El ContentResolver es el puente seguro para acceder a los archivos del sistema
+            
             context.contentResolver.openOutputStream(uri)?.use { outputStream ->
                 OutputStreamWriter(outputStream).use { writer ->
                     writer.write(contenido)
                 }
             }
-            true // Retornamos true si la exportación fue exitosa
+            true /
         } catch (e: Exception) {
             e.printStackTrace()
-            // Área de mejora: Manejar el fallo y notificar a la capa superior (UI)
+            
             false
         }
     }
