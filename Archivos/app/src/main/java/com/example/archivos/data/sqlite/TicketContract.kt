21831e20a6bcf18230998334d0afaba9b9c3769f
Luis Zarate <126049010+GenoXZG@users.noreply.github.com>
2026-05-28 00:32:01 -0600

    Update TicketContract.kt

--- Archivos/app/src/main/java/com/example/archivos/data/sqlite/TicketContract.kt
+++ Archivos/app/src/main/java/com/example/archivos/data/sqlite/TicketContract.kt
@@ -3,7 +3,6 @@ package com.example.archivos.data.sqlite
 import android.provider.BaseColumns
 
 object TicketContract {
-    // Definimos el contenido de la tabla de nuestra bitácora
     object TicketEntry : BaseColumns {
         const val TABLE_NAME = "tickets"
         const val COLUMN_NAME_TITLE = "titulo"
